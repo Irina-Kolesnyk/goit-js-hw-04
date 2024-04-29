8d44ad0162b8606862cdc584dfa900ae184591dd
Irina Kolesnyk <157540775+Irina-Kolesnyk@users.noreply.github.com>
2024-04-29 11:47:50 +0300

    add correct console

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,7 @@ const profile = {
     username: "Jacob",
     playTime: 300,
          
-    changeUserName(newName) {
+    changeUsername(newName) {
     this.username = newName;
     },
         
@@ -13,8 +13,12 @@ const profile = {
     getInfo() {
     return `${this.username} has ${this.playTime} active hours!`;
     },
-         
+       
 };
 
 console.log(profile.getInfo());
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
